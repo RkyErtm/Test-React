db78418ac049d80c40ab6c52abc15e5d425352eb
RukiyeErtem <rkyertm1209@gmail.com>
2024-09-24 21:39:45 +0300

    *

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import {Component} from 'react';
 import {Text, View} from 'react-native';
-import stlyes from './styles';
+import stlyes from './styles'; //* dışardaki css'i buraya import ederek kullandık.
 export default class App extends Component {
   render(): any {
     return (
