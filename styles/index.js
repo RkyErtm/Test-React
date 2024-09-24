c9b025debf6b15bf912485e110a9774f52533737
RukiyeErtem <rkyertm1209@gmail.com>
2024-09-24 21:39:22 +0300

    styles added

---
+++ styles/index.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const stlyes = StyleSheet.create({
+  container: {},
+  item: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default stlyes;
