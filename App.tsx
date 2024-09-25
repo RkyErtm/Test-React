171d23baab9504d4f00c71a8a5386a4473b4c048
RukiyeErtem <rkyertm1209@gmail.com>
2024-09-25 17:47:45 +0300

    flex style added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,46 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {Component} from 'react';
-import {Text, View} from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import stlyes from './styles'; //* dışardaki css'i buraya import ederek kullandık.
 export default class App extends Component {
   render(): any {
     return (
-      <View style={stlyes.container}>
-        <Text style={stlyes.item}>fdsfds</Text>
+      // * flex - yatay ekranda ekranı doldurmayı sağlar.
+      <View style={[toolbar.frame]}>
+        <View style={toolbar.bgSearch}>
+          <View style={toolbar.searchInput} />
+          <Text style={toolbar.label}>Ara</Text>
+        </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>
+            Test
+          </Text>
+        </View>
       </View>
+
     );
   }
 }
+
+const toolbar = StyleSheet.create({
+  frame: {
+    flex: 1
+  },
+  bgSearch: {
+    backgroundColor: "red",
+    height: 60,
+    flexDirection: 'row',
+    paddingHorizontal: 10
+  },
+  searchInput: {
+    backgroundColor: '#fff',
+    height: 30,
+    marginTop: 15,
+    flex: 1
+  },
+  label: {
+    marginTop: 18,
+    paddingLeft: 10,
+    color: "#fff"
+  }
+});
